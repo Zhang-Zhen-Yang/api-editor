072d58ace173cb4691705625ece0e552bb66949b
zzy <1097088392@qq.com>
2017-11-22 17:35:59 +0800

    resize组件修改

--- VIEW/src/component/resize/resize.vue
+++ VIEW/src/component/resize/resize.vue
@@ -5,13 +5,15 @@
     @mousemove="resizeMove($event)" 
     @mouseup="resizeEnd($event)" 
     @mouseleave="resizeEnd($event)" 
+    @drop="drop($event)" 
+    @dragstart="dragstart($event)" 
     ref="resizeWrap">
     <div class="v-resize-first" :style="{width:first}">
       <slot name="first"></slot>
     </div><div class="v-resize-last" :style="{width:last}">
       <slot name="last"></slot>
     </div>
-    <div class="v-resize-trigger trigger-last" :style="{left:triggerLeft}" @mousedown="resizeStart($event)"></div>
+    <div class="v-resize-trigger trigger-last" draggable="false" :style="{left:triggerLeft}" @mousedown="resizeStart($event)"></div>
   </div>
 </template>
 
@@ -80,12 +82,13 @@ export default {
 
     },
     dragCss(){
-      return this.resizeStartMsg.starting?{'user-select':'none'}:{};      
+      return {};//this.resizeStartMsg.starting?{'user-select':'none'}:{};      
     },
   },
   methods:{
+    // 调整开始
     resizeStart(e){
-      console.debug(e);
+      // console.debug(e);
       let resizeStartRatioStart = parseFloat(this.first);
       let resizeEndRatioLast = parseFloat(this.last);
       this.resizeStartMsg.starting = true;
@@ -93,29 +96,52 @@ export default {
       this.resizeStartMsg.resizeStartRatioStart = resizeStartRatioStart;
       this.resizeStartMsg.resizeStartRatioLast = resizeEndRatioLast;
     },
+    // 调整中
     resizeMove(e){
       if(this.resizeStartMsg.starting){
         let distance = e.pageX - this.resizeStartMsg.pageX;
         if(this.resizeType == 'fixedStart'){
-          this.first = distance + this.resizeStartMsg.resizeStartRatioStart+'px';
-          this.last = `calc( 100% - ${distance + this.resizeStartMsg.resizeStartRatioStart}px )`;
-          this.triggerLeft = distance + this.resizeStartMsg.resizeStartRatioStart-5+'px';
+          let clientWidth = this.$refs.resizeWrap.clientWidth;
+          
+          let first = distance + this.resizeStartMsg.resizeStartRatioStart+'px';
+          let last = `calc( 100% - ${distance + this.resizeStartMsg.resizeStartRatioStart}px )`;
+          if(first>0 && first<clientWidth){
+            this.first = first;
+            this.last = last;
+            this.triggerLeft = distance + this.resizeStartMsg.resizeStartRatioStart-5+'px';
+          }
         }else if(this.resizeType == 'fixedEnd'){
-          this.last = -distance + this.resizeStartMsg.resizeStartRatioLast+'px';
-          this.first = `calc( 100% - ${-distance + this.resizeStartMsg.resizeStartRatioLast}px )`;
-          this.triggerLeft = `calc( 100% - ${-distance + this.resizeStartMsg.resizeStartRatioLast + 5}px )`;
+          let last = -distance + this.resizeStartMsg.resizeStartRatioLast+'px';
+          let first = `calc( 100% - ${-distance + this.resizeStartMsg.resizeStartRatioLast}px )`;
+          if(last>0 && last<clientWidth){
+            this.last = last;
+            this.first = first;
+            this.triggerLeft = `calc( 100% - ${-distance + this.resizeStartMsg.resizeStartRatioLast + 5}px )`;
+          }
         }else if(this.resizeType == 'ratio'){
           let distancePre =  (distance/this.$refs.resizeWrap.clientWidth)*100;
-          this.first = this.resizeStartMsg.resizeStartRatioStart + distancePre + '%';
-          this.last = this.resizeStartMsg.resizeStartRatioLast - distancePre + '%';
-          this.triggerLeft = `calc( ${this.first} - 5px )`;
+          let first = this.resizeStartMsg.resizeStartRatioStart + distancePre;
+          let last = this.resizeStartMsg.resizeStartRatioLast - distancePre;
+          if(first>0 && first<100){
+            this.first = first + '%';
+            this.last = last + '%';
+            this.triggerLeft = `calc( ${this.first} - 5px )`;
+          }
           // console.log(this.$refs.resizeWrap.clientWidth);
         }
         // console.log(distance);
       }
     },
+    // 调整结束
     resizeEnd(e){
       this.resizeStartMsg.starting = false;
+    },
+    drop(){
+      // console.log('drop-----------------------------------------------drop');
+    },
+    dragstart(){
+      this.resizeStartMsg.starting = false;
+      // console.log('dragstart-----------------------------------------------dragstart');
     }
   },
   created(){
